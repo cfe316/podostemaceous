33d2884c148ed00d43be016ed5dbade1e3be0b68
Jacob A. Schwartz <jacob@jaschwartz.net>
2019-03-02 14:07:20 -0500

    Source physical constants from scipy.constants

--- podostemaceous/constants.py
+++ podostemaceous/constants.py
@@ -1,11 +1,8 @@
 # Some constants for general use with simulations.
 
-# "CODATA recommended values of the 
-#   fundamental physical constants: 2014"
-# Rev. Mod. Phys., Vol. 88, No. 3, July–September 2016
-# Equation 247
-kB = 1.3806513e-23 # J / K 
-# Table XXXII
-eV = 1.6021766208e-19 # J
-u = 1.660539040e-27 # kg
+from scipy.constants import Boltzmann as kB
+from scipy.constants import eV
+from scipy.constants import physical_constants
 
+u, _, _ = physical_constants['unified atomic mass unit']
+del physical_constants
